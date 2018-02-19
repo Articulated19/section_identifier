6d5254c7970a9b92a8152a18c6edc9ab4276a2d7
Sebastian Nilsson <sebbehebbe@users.noreply.github.com>
2018-02-19 15:47:12 +0100

    Temporary version object oriented (#1)
    * Section listener now grabs position from truck state
    
    * Stop the truck
    
    * Made it object oriented. Communication works
    
    * Removed test comment

--- scripts/section_identifier.py
+++ scripts/section_identifier.py
@@ -4,25 +4,32 @@ from custom_msgs.msg import *
 from custom_msgs.srv import *
 from std_msgs.msg import String
 
+
 class SectionIdentifier:
+
     def __init__(self):
-        self.pub = rospy.Publisher('/section_identifier', String, queue_size=10)
-
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # node are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
         rospy.init_node('section_identifier', anonymous=True)
-        rospy.Subscriber('truck_state', TruckState, self.truckStateHandler)
-        
-    def truckStateHandler(self, data):
-         # x > 600 || x 1500 
+
+        self.pub = rospy.Publisher('section_identifier', String, queue_size=10)
+
+        rospy.Subscriber("truck_state", TruckState, self.callback)
+
+    def callback(self, data):
+        # x > 600 || x 1500
         # y > 5900 || y < 7000
+        x = data.p.x
+        y = data.p.y
+        rospy.loginfo("x = %s, y = %s)", String(data.p.x), String(data.p.y))
+        if 600 < x < 1500 and 5900 < y < 7000:
+            self.pub.publish("STAPH THE TRUCK")
 
-        self.pub.publish(data.p.x)
-        self.pub.loginfo(data.p.y)
-
-    def spin():
-        rate = rospy.Rate(10) # 10hz
-        while not rospy.is_shutdown():
-            rate.sleep()
 
 if __name__ == '__main__':
     s = SectionIdentifier()
-    # s.spin()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
