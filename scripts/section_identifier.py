c36e222aeafc23b10cdd185e6163ded26a610261
Sebastian Nilsson <sebastian@typeof.nu>
2018-02-21 14:09:51 +0100

    Fix to avoid spamming

--- scripts/section_identifier.py
+++ scripts/section_identifier.py
@@ -14,10 +14,12 @@ class SectionIdentifier:
         # name for our 'listener' node so that multiple listeners can
         # run simultaneously.
         rospy.init_node('section_identifier', anonymous=True)
+        self.isInSection = False
 
-        self.pub = rospy.Publisher('section_identifier', String, queue_size=10)
+        self.pub = rospy.Publisher('section_identifier', String, queue_size=0)
 
         rospy.Subscriber("truck_state", TruckState, self.callback)
+        rospy.Subscriber("section_lock", String, self.release_callback)
 
     def callback(self, data):
         # x > 600 || x 1500
@@ -25,8 +27,14 @@ class SectionIdentifier:
         x = data.p.x
         y = data.p.y
         rospy.loginfo("x = %s, y = %s)", String(data.p.x), String(data.p.y))
-        if 600 < x < 1500 and 5900 < y < 7000:
+        if 600 < x < 1500 and 5900 < y < 7000 and not self.isInSection:
+            self.isInSection = True
             self.pub.publish("STAPH THE TRUCK")
+
+
+    def release_callback(self, data):
+        if data.data == "release":
+            self.isInSection = False
 
 
 if __name__ == '__main__':
