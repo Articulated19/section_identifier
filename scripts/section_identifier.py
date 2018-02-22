63d2fe5ac4cbedf3a62aa5b1a1fb074be264d4e5
BurkinGunke <burkingunke@hotmail.com>
2018-02-22 14:17:48 +0100

    Coordinates if case

--- scripts/section_identifier.py
+++ scripts/section_identifier.py
@@ -27,10 +27,17 @@ class SectionIdentifier:
         x = data.p.x
         y = data.p.y
         rospy.loginfo("x = %s, y = %s)", String(data.p.x), String(data.p.y))
-        if 600 < x < 1500 and 5900 < y < 7000 and not self.isInSection:
+        # 600 < x < 1800 and 5600 < y < 7300     #  Intersection 1
+        # 600 < x < 1800 and 3200 < y < 4700     #  Intersection 2
+        # 1900 < x < 3800 and 2550 < y < 5400    #  Roundabout
+        # 2600 < x < 3800 and 5600 < y < 7400    #  Intersection 3
+
+        if (600 < x < 1800 and 5600 < y < 7300) or \
+            (600 < x < 1800 and 3200 < y < 4700) or \
+            (1900 < x < 3800 and 2550 < y < 5400) or \
+            (2600 < x < 3800 and 5600 < y < 7400) and not self.isInSection:
             self.isInSection = True
             self.pub.publish("STAPH THE TRUCK")
-
 
     def release_callback(self, data):
         if data.data == "release":
