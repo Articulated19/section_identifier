e22282d93588db64a58b474ec23f6757fec792a4
BurkinGunke <burkingunke@hotmail.com>
2018-04-25 10:57:29 +0200

    Fixed direction function giving wrong directions. Updated section coordinates.

--- scripts/section_identifier.py
+++ scripts/section_identifier.py
@@ -50,15 +50,20 @@ class SectionIdentifier:
         intersection3 = []
         roundabout = []
 
+        # 1# 600 < x < 1800 and 5600 < y < 7300     #  Intersection 1
+        # 2# 600 < x < 1800 and 3200 < y < 4800    #  Intersection 2
+        # 3# 2000 < x < 3850 and 2550 < y < 5600    #  Roundabout
+        # 4# 2600 < x < 3850 and 5600 < y < 7300    #  Intersection 3
+
         for p in path:
             if 600 < p.x < 1800 and 5600 < p.y < 7300:
                 intersection1.append(p)
-            elif 600 < p.x < 1800 and 3200 < p.y < 4700:
+            elif 600 < p.x < 1800 and 3200 < p.y < 4800:
                 intersection2.append(p)
-            elif 1900 < p.x < 3850 and 2550 < p.y < 5500:
-                intersection3.append(p)
-            elif 2600 < p.x < 3850 and 5600 < p.y < 7400:
+            elif 2000 < p.x < 3850 and 2550 < p.y < 5600:
                 roundabout.append(p)
+            elif 2600 < p.x < 3850 and 5600 < p.y < 7300:
+                intersection3.append(p)
 
         return {
             "intersection1": intersection1,
@@ -83,13 +88,12 @@ class SectionIdentifier:
 
         if 600 < x < 1800 and 5600 < y < 7300:
             self.pub.publish("Intersection_1")
-        elif 600 < x < 1800 and 3200 < y < 4700:
+        elif 600 < x < 1800 and 3200 < y < 4800:
             self.pub.publish("Intersection_2")
-        elif 1900 < x < 3850 and 2550 < y < 5500:
+        elif 2000 < x < 3850 and 2550 < y < 5600:
             self.pub.publish("Roundabout")
-        elif 2600 < x < 3850 and 5600 < y < 7400:
+        elif 2600 < x < 3850 and 5600 < y < 7300:
             self.pub.publish("Intersection_3")
-
 
 if __name__ == '__main__':
     s = SectionIdentifier()
